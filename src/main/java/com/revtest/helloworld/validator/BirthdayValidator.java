78f27aa11fb95b35ea2b9eebda01a5788457b360
arulkumarkandasamy <karul43@yahoo.co.in>
2019-04-22 22:55:20 +0100

    fixing errors

--- src/main/java/com/revtest/helloworld/validator/BirthdayValidator.java
+++ src/main/java/com/revtest/helloworld/validator/BirthdayValidator.java
@@ -1,12 +1,13 @@
 package com.revtest.helloworld.validator;
 
 
-import org.springframework.stereotype.Component;
-import org.springframework.validation.Errors;
-
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
 
 @Component
 public class BirthdayValidator{
@@ -15,7 +16,11 @@ public class BirthdayValidator{
 		DateFormat format = new SimpleDateFormat(pattern);
 		format.setLenient(false);
 		try {
-            format.parse(dateOfBirth);
+            Date birthdate = format.parse(dateOfBirth);
+            Date today = new Date();
+            if (birthdate.compareTo(today)>=0) {
+            	errors.reject("Date " + dateOfBirth + "must be a date before the today date");
+            }
         } catch (ParseException e) {
         	errors.reject("Date " + dateOfBirth + " is not valid according to " +
                     ((SimpleDateFormat) format).toPattern() + " pattern.");
