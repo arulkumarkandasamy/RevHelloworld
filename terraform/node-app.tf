ff60ac8c95c1f750374ecf53ff1cde47e742fc0f
arulkumarkandasamy <karul43@yahoo.co.in>
2019-04-22 17:29:25 +0100

    fixing errors

--- terraform/node-app.tf
+++ terraform/node-app.tf
@@ -127,7 +127,7 @@ resource "aws_elb" "elb1" {
   }
 
   cross_zone_load_balancing = true
-  idle_timeout = 400
+  idle_timeout = 40000
   connection_draining = true
   connection_draining_timeout = 400
 
